95c3d9272c1802e3566e940b1e417c8dc7be3fcf
iliass bargache <ibargache6@gmail.com>
2024-12-13 07:01:34 +0100

    all

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,44 @@
 #!/usr/bin/python3
-"""Module defining isWinner function."""
+"""
+Prime Game Module: Defines function that determines the winner after a certain
+number of rounds of playing the Prime Game.
+"""
 
 
 def isWinner(x, nums):
-    """Function to get who has won in prime game"""
-    mariaWinsCount = 0
-    benWinsCount = 0
+    """isWinner
 
-    for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
+    Determine who is the winner after a certain number of rounds.
 
-        if not primesSet:
-            benWinsCount += 1
+    Argumnets:
+        x (int): The number of rounds
+        nums (List[int]): List of all the n's for each round.
+
+    Return:
+        (str): The name of the player who wins.
+    """
+    if not nums or x < 1:  # if nums is empty or x is less than 1
+        return None
+    n = max(nums)  # get the max value in nums list
+    sieve = [True for _ in range(max(n + 1, 2))]  # create a sieve of True val.
+    for i in range(2, int(pow(n, 0.5)) + 1):  # loop through the sieve
+        if not sieve[i]:  # if the value is False (not prime)
             continue
+        for j in range(i*i, n + 1, i):  # loop through the sieve
+            sieve[j] = False  # set the value to False (not prime)
 
-        isMariaTurns = True
+    sieve[0] = sieve[1] = False  # set 0 and 1 to False (not prime)
+    count = 0  # initialize count to 0 (Maria's score)
+    for i in range(len(sieve)):  # loop through the sieve and count the primes
+        if sieve[i]:  # if the value is True (prime)
+            count += 1
+        sieve[i] = count  # set the value to the count of primes
 
-        while(True):
-            if not primesSet:
-                if isMariaTurns:
-                    benWinsCount += 1
-                else:
-                    mariaWinsCount += 1
-                break
-
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
-
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
-
-            isMariaTurns = not isMariaTurns
-
-    if mariaWinsCount > benWinsCount:
-        return "Winner: Maria"
-
-    if mariaWinsCount < benWinsCount:
-        return "Winner: Ben"
-
-    return None
-
-
-def is_prime(n):
-    """Returns True if n is prime, else False."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def primes_in_range(start, end):
-    """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [n for n in range(start, end+1) if is_prime(n)]
-    return primes
+    player1 = 0
+    for n in nums:  # loop through the nums list and count Maria's wins
+        player1 += sieve[n] % 2 == 1  # if Maria's score is odd, she wins
+    if player1 * 2 == len(nums):  # if Maria wins half the rounds
+        return None
+    if player1 * 2 > len(nums):  # if Maria wins more than half the rounds
+        return "Maria"  # Maria wins
+    return "Ben"  # Ben wins
